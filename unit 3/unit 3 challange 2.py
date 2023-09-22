8b0e0a4ab94cbcbc87e530133aef3d2be9438263
boobalan3 <144766750+boobalan3@users.noreply.github.com>
2023-09-22 13:30:53 +0530

    Add files via upload

---
+++ unit 3/unit 3 challange 2.py
@@ -0,0 +1,25 @@
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the student list in descending order based on CGPA
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
+
+# Test the function with different input lists of students
+students = [
+    Student("Alice", "A101", 3.9),
+    Student("Bob", "B102", 3.5),
+    Student("Charlie", "C103", 4.0),
+    Student("David", "D104", 3.8),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+
